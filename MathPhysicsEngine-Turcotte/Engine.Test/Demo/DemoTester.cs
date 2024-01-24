2b925b9577bf390a48f8cf59b4803a284e74eb5f
RiverCrusader <jturcotte5@nait.ca>
2024-01-24 16:29:52 -0500

    rebuild thing

---
+++ MathPhysicsEngine-Turcotte/Engine.Test/Demo/DemoTester.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namesapces
+using NUnit.Framework;
+using Engine.Mathematics;
+#endregion
+
+namespace Engine.NunitTests.Demo
+{
+	[TestFixture]
+	public class DemoTester
+	{
+		[Test,
+		TestCase(0, 0, 3, 4, 5), // Instructor Data - MUST NOT DELETE OR MODIFY
+		TestCase(0, 0, 6, 8, 10) // Student Data - MUST Change
+	]
+		public void TestSegmentLenthPoint2D(double aX, double aY, double bX, double bY, double expected)
+		{
+			// Create Objects for the test
+			Eng_Point2D a = new Eng_Point2D(aX, aY);
+			Eng_Point2D b = new Eng_Point2D(bX, bY);
+			// Performing the test
+			double actual = a.SegmentLength(b);
+			Assert.AreEqual(expected, Math.Round(actual, 4));
+
+		}//eom
+
+		public static IEnumerable<Object[]> MidPointData()
+		{
+			// Instructor Data - MUST NOT DELETE OR MODIFY
+			// 1st new Eng_Point2D is the point A
+			// 2nd new Eng_Point2D is the point B
+			// 3rd new Eng_Point2D is the expected outcome
+			yield return new Object[]
+			{
+				new Eng_Point2D(3, 4),      // point a
+                new Eng_Point2D(6, 9),      // point b
+                new Eng_Point2D(4.5, 6.5)   // expected midpoint
+            };
+
+			// Student Data - MUST Change
+			// 1st new Eng_Point2D is the point A
+			// 2nd new Eng_Point2D is the point B
+			// 3rd new Eng_Point2D is student expected outcome
+			yield return new Object[]
+			{
+				new Eng_Point2D(3, 4),      // point a
+                new Eng_Point2D(6, 9),      // point b
+                new Eng_Point2D(4.5, 5.5)   // expected midpoint
+            };
+		}//eom
+
+		[Test, TestCaseSource(nameof(MidPointData))]
+		public void TestMidpointPoint2D(Eng_Point2D a, Eng_Point2D b, Eng_Point2D expected)
+		{
+			// Performing the test (no setup required)
+			Eng_Point2D actual = a.MidPoint(b);
+			// Assert - did we get back the correct answer
+			Assert.AreEqual(expected.X, Math.Round(actual.X, 4));
+			Assert.AreEqual(expected.Y, Math.Round(actual.Y, 4));
+
+		}//eom
+	}//eoc
+}//eon
---
+++ MathPhysicsEngine-Turcotte/Engine/Mathematics/Eng_Point2D.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Mathematics
+{
+    public class Eng_Point2D
+    {
+        // Properties
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        // Empty Constructor
+        public Eng_Point2D()
+        {
+            X = 0;
+            Y = 0;
+        }//eom
+
+        // Greedy Constructor
+        public Eng_Point2D(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }//eom
+
+		#region Class Methods
+		public double SegmentLength(Eng_Point2D b)
+		{
+			return Math.Sqrt(Math.Pow(b.X - X, 2) + Math.Pow(b.Y - Y, 2));
+		}//eom
+
+		public Eng_Point2D MidPoint(Eng_Point2D b)
+		{
+			return new Eng_Point2D(0.5 * (X + b.X), 0.5 * (Y + b.Y));
+		}//eom
+		#endregion
+
+		#region Overload Operators
+		#region Complier Warning Fix
+		// the following two methods are to remove the CS0660 and CS0661 compiler warnings
+		public override bool Equals(object obj)
+		{
+			return true;
+		}//eom
+		public override int GetHashCode()
+		{
+			return 0;
+		}//eom
+		#endregion
+		// the == and != must both be coded or you get a compile error
+		public static bool operator ==(Eng_Point2D a, Eng_Point2D b)
+		{
+			return a.X == b.X && a.Y == b.Y;
+		}//eom
+
+		public static bool operator !=(Eng_Point2D a, Eng_Point2D b)
+		{
+			return a.X != b.X || a.Y != b.Y;
+		}//eom
+
+		public static Eng_Point2D operator *(double s, Eng_Point2D p)
+		{
+			return new Eng_Point2D(s * p.X, s * p.Y);
+		}//eom
+		#endregion
+	}//eoc
+}//eon
