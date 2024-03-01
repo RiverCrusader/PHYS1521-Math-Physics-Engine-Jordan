a3f887924a5bdba2babbd7b724c85982b9f77504
RiverCrusader <144395370+RiverCrusader@users.noreply.github.com>
2024-03-01 10:40:16 -0700

    most 4x4 operators are done

--- MathPhysicsEngine-Turcotte/Engine/Mathematics/Eng_Matrix4x4.cs
+++ MathPhysicsEngine-Turcotte/Engine/Mathematics/Eng_Matrix4x4.cs
@@ -234,13 +234,66 @@ namespace Engine.Mathematics
         #endregion
 
         //4.a - Equality of two 4x4 matrices.
-       
+        public static bool operator ==(Eng_Matrix4x4 a, Eng_Matrix4x4 b)
+        {
+            return
+            a.M11 == b.M11 &&
+            a.M12 == b.M12 &&
+            a.M13 == b.M13 &&
+            a.M14 == b.M14 &&
+
+            a.M21 == b.M21 &&
+            a.M22 == b.M22 &&
+            a.M23 == b.M23 &&
+            a.M24 == b.M24 &&
+
+            a.M31 == b.M31 &&
+            a.M32 == b.M32 &&
+            a.M33 == b.M33 &&
+            a.M34 == b.M34 &&
+
+            a.M41 == b.M41 &&
+            a.M42 == b.M42 &&
+            a.M43 == b.M43 &&
+            a.M44 == b.M44;
+        }
+
+
         //4.b - Inequality of two 4x4 matrices.
-        
+        public static bool operator !=(Eng_Matrix4x4 a, Eng_Matrix4x4 b)
+        {
+            return
+            a.M11 != b.M11 ||
+            a.M12 != b.M12 ||
+            a.M13 != b.M13 ||
+            a.M14 != b.M14 ||
+
+            a.M21 != b.M21 ||
+            a.M22 != b.M22 ||
+            a.M23 != b.M23 ||
+            a.M24 != b.M24 ||
+
+            a.M31 != b.M31 ||
+            a.M32 != b.M32 ||
+            a.M33 != b.M33 ||
+            a.M34 != b.M34 ||
+
+            a.M41 == b.M41 ||
+            a.M42 == b.M42 ||
+            a.M43 == b.M43 ||
+            a.M44 == b.M44;
+        }
         //4.c - Scale a 4x4 matrix.
-        
+        public static Eng_Matrix4x4 operator *(Eng_Matrix4x4 a, double scalar)
+        {
+            //multiply each number by scalar
+            return new Eng_Matrix4x4(a.M11 * scalar, a.M12 * scalar, a.M13 * scalar, a.M14 * scalar,
+                                     a.M21 * scalar, a.M22 * scalar, a.M23 * scalar, a.M24 * scalar,
+                                     a.M31 * scalar, a.M32 * scalar, a.M33 * scalar, a.M34 * scalar,
+                                     a.M41 * scalar, a.M42 * scalar, a.M43 * scalar, a.M44 * scalar);
+        }
         //4.d - Multiply two 4x4 matrices.
-        
+
         #endregion
     }//eoc
 }//eon
